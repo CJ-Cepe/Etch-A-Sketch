593a13652da850ef5e3335024d3c3b8f5b3f0ab6
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-05-27 22:59:06 +0800

    change color icons when color is picked

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -4,8 +4,13 @@ const elements = {
     tiles: document.querySelector('.tile'),
     numTilesSlider: document.querySelector('#num-tiles-slider'),
     gridOpacitySlider: document.querySelector('#grid-opacity-slider'),
-    colorCont: document.querySelector('#color-cont')
+    colorCont: document.querySelector('#color-cont'),
 
+    solidIcon: document.querySelector('.solid'),
+    lightDarkIcon: document.querySelector('.light-dark'),
+    darkLightIcon: document.querySelector('.dark-light'),
+    complementaryIcon: document.querySelector('.complementary'),
+    triadicIcon:document.querySelector('.triadic')
 }
 
 const initial = {
@@ -14,7 +19,6 @@ const initial = {
 }
 
 createTiles(10)
-
 
 elements.numTilesSlider.addEventListener('input', ()=>{
     removeTiles()
@@ -34,6 +38,7 @@ elements.gridOpacitySlider.addEventListener('input', ()=>{
 
 elements.colorCont.addEventListener('input', function(){
     initial.BASE_COLOR = elements.colorCont.value
+    updateIconColors()
 })
 
 //Procedurally creates tiles given number
@@ -68,9 +73,42 @@ function removeTiles(){
 }
 
 function updateIconColors(){
-    solid
-    light-dark
-    dark-light
-    complementary
-    triadic
+    elements.solidIcon.style.backgroundColor = initial.BASE_COLOR
+    console.log(initial.BASE_COLOR[0])
+
+    let rgba = hexToRgb(initial.BASE_COLOR)
+    rgba = 'rgba(' + rgba + ','
+    console.log(rgba)
+    elements.lightDarkIcon.style.background = `linear-gradient(135deg, ${rgba}0.25) 0%, ${rgba}1) 100%)`
+    console.log(`linear-gradient(135deg, ${rgba} 0.25) 0%, ${rgba} 1) 100%);`)
+
+    elements.darkLightIcon.style.background = `linear-gradient(135deg, ${rgba}1) 0%, ${rgba}0) 100%)`
+
+    let complementary = getComplementary(initial.BASE_COLOR)
+    elements.complementaryIcon.style.background = `linear-gradient(135deg, ${initial.BASE_COLOR} 50%, ${complementary} 50%)`
+    console.log(`linear-gradient(135deg, ${initial.BASE_COLOR} 50%, ${complementary} 50%)`)
+}
+
+function hexToRgb(hex){
+    hex = hex.substring(1)
+    if(hex.length != 6){
+        console.log('not equal to 6')
+    }
+    let aRgbHex = hex.match(/.{1,2}/g);
+    let aRgb = [
+        parseInt(aRgbHex[0], 16),
+        parseInt(aRgbHex[1], 16),
+        parseInt(aRgbHex[2], 16)
+    ];
+    return aRgb
+}
+
+function getComplementary(color = ''){
+    let colorPart = color.slice(1);
+    let ind = parseInt(colorPart, 16);
+    let iter = ((1 << 4 * colorPart.length) - 1 - ind).toString(16);
+    while(iter.length < colorPart.length) {
+        iter = '0'+iter
+    }
+    return '#' + iter;
 }
